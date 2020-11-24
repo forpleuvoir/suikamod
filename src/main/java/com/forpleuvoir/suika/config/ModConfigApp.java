70509e728a57d07bd6923960f91b1a3b8846c81d
forpleuvoir <cao/4321>
2020-11-24 15:27:51 +0800

    suikalib 升级

--- src/main/java/com/forpleuvoir/suika/config/ModConfigApp.java
+++ src/main/java/com/forpleuvoir/suika/config/ModConfigApp.java
@@ -6,8 +6,6 @@ import com.forpleuvoir.suikalib.config.annotation.SuikaConfig;
 import net.minecraft.client.MinecraftClient;
 
 import java.io.File;
-import java.util.HashSet;
-import java.util.Set;
 
 /**
  * @author forpleuvoir
@@ -16,7 +14,7 @@ import java.util.Set;
  * @class_name ModConfigApp
  * @create_time 2020/11/11 18:53
  */
-@SuikaConfig(value = "suika", packages = "com.forpleuvoir.suika.config")
+@SuikaConfig
 public class ModConfigApp {
     public static final File MAIN_DIR = MinecraftClient.getInstance().runDirectory;
     public static final String FILE_DIR = MAIN_DIR + "/config/suika/";
@@ -26,8 +24,6 @@ public class ModConfigApp {
 
     public static void init() {
         File file =new File(FILE_DIR+"suikamod_config.json");
-        Set<Class<?>> classes = new HashSet<>();
-        classes.add(ModConfig.class);
-        SuikaConfigApp.init(ModConfigApp.class, classes,file);
+        SuikaConfigApp.init(ModConfigApp.class,file);
     }
 }
